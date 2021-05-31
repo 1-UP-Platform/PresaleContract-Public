9018840d9b1fedb58cf00806fefc9d495b18b23f
1-UPToken <83342072+1-up-platform@users.noreply.github.com>
2021-05-31 05:20:07 -0700

    Delete contracts directory

--- contracts/interfaces/ILiquidityProvider.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: Unlicensed
-pragma solidity 0.8.0;
-
-
-interface ILiquidityProvider {
-    function addLiquidity() external;
-    function emergencyWithdraw(address payable receiver) external;
-    function recoverERC20(address lpTokenAddress, address receiver) external;
-}
